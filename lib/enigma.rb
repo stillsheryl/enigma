59a603bf7989f8869f32f620a1a27cf421d9f8dc
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-18 09:36:12 -0600

    Add todays date to string method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,14 @@
+require 'date'
+
 class Enigma
 
   def generate_random_key
     "%05d" % rand(99999)
+  end
+
+  def todays_date_to_string
+    date = Date.today
+    date.strftime("%d%m%y")
   end
 
 end
