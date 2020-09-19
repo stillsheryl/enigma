73f940142543b93b7ce554363e6a13017985257c
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-19 11:02:37 -0600

    Add generate random key and todays date methods

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,8 +1,17 @@
 class KeyGenerator
   attr_reader :key, :date
-  def initialize(key, date)
+  def initialize(key = generate_random_key, date = todays_date_to_string)
     @key = key
     @date = date
+  end
+
+  def generate_random_key
+    "%05d" % rand(99999)
+  end
+
+  def todays_date_to_string
+    date = Date.today
+    date.strftime("%d%m%y")
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,13 +11,6 @@ class EnigmaTest < MiniTest::Test
     assert_equal ({}), enigma.date_offset
   end
 
-  def test_generate_random_key
-    enigma = Enigma.new
-
-    enigma.stubs(:rand).returns(4523)
-
-    assert_equal "04523", enigma.generate_random_key
-  end
 
   def test_key_shift
     enigma = Enigma.new
@@ -31,12 +24,7 @@ class EnigmaTest < MiniTest::Test
     assert_equal expected, enigma.key_shift("04523")
   end
 
-  def test_todays_date_to_string
-    enigma = Enigma.new
-    Date.stubs(:today).returns(Date.new(2020,9,18))
 
-    assert_equal "180920", enigma.todays_date_to_string
-  end
 
   def test_calculate_offset_from_date
     enigma = Enigma.new
