2fec2599f24beae15a1e6896ba5fc84bdaf2da5c
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-19 16:36:17 -0600

    Add functionality to use CLI to run encrypt

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,14 +2,17 @@ require_relative 'enigma.rb'
 
 handle = File.open(ARGV[0], "r")
 
-incoming_message = handle.read
+incoming_message = handle.read.to_s.chomp
 
 handle.close
 
+enigma = Enigma.new
 translated = enigma.encrypt(incoming_message)
 
 writer = File.open(ARGV[1], "w")
 
-writer.write "Created 'encrypted.txt' with the key #{translated[:key]} and date #{translated[:date]}"
+writer.write translated[:encryption]
+
+puts "Created 'encrypted.txt' with the key #{translated[:key]} and date #{translated[:date]}"
 
 writer.close
