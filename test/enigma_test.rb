22c7a8c0074175ad1dd4141aadf2260e64d8baf3
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-19 11:16:21 -0600

    Add test to generate alphabet shifts

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,4 @@
 require_relative 'test_helper'
-require 'mocha/minitest'
 
 class EnigmaTest < MiniTest::Test
 
@@ -7,24 +6,7 @@ class EnigmaTest < MiniTest::Test
     enigma = Enigma.new
 
     assert_instance_of Enigma, enigma
-    assert_equal ({}), enigma.key_shifts
-    assert_equal ({}), enigma.date_offset
   end
 
-  def test_alphabet
-    enigma = Enigma.new
-
-    assert_equal ["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f"], enigma.alphabet(6)
-  end
-
-  def test_alphabets_for_encoding
-    enigma = Enigma.new
-    enigma.generate_random_key
-    enigma.key_shift("04523")
-    enigma.calculate_offset_from_date("080820")
-    enigma.final_shifts
-
-    assert_equal [["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f"], ["w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"], ["z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"],["x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w"]], enigma.alphabets_for_encoding([6, 49, 52, 23])
-  end
 
 end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -73,4 +73,19 @@ class KeyGeneratorTest < MiniTest::Test
     assert_equal [6, 49, 52, 23], key_generator.final_shifts_array
   end
 
+  def test_alphabet
+    key_generator = KeyGenerator.new("04523", "080820")
+
+    assert_equal ["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f"], key_generator.alphabet(6)
+  end
+
+  def test_alphabets_for_encoding
+    key_generator = KeyGenerator.new("04523", "080820")
+    key_generator.key_shift("04523")
+    key_generator.calculate_offset_from_date("080820")
+    key_generator.final_shifts
+
+    assert_equal [["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f"], ["w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"], ["z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"],["x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w"]], key_generator.alphabets_for_encoding([6, 49, 52, 23])
+  end
+
 end
