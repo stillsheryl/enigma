7b61a7b7024b815b0468d458bfe3832cbd01e9f2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-20 11:32:32 -0600

    Add alphabets for encoding method

--- lib/encrypt_key.rb
+++ lib/encrypt_key.rb
@@ -5,4 +5,9 @@ class EncryptKey < KeyGenerator
     super(key, date)
   end
 
+  def alphabets_for_encoding(key, date)
+    final_shift = final_shifts(key, date)
+    [alphabet(final_shift[0]), alphabet(final_shift[1]), alphabet(final_shift[2]), alphabet(final_shift[3])]
+  end
+
 end
