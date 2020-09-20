164cc4470141d93b33d7e915f222e7f089a527d7
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-20 11:43:41 -0600

    Add test alphabets for decoding

--- test/decrypt_key_test.rb
+++ test/decrypt_key_test.rb
@@ -16,4 +16,14 @@ class DecryptKeyTest < MiniTest::Test
     assert_equal "080820", encrypt_key.date
   end
 
+  def test_alphabets_for_decoding
+    decrypt_key = DecryptKey.new("04523", "080820")
+    key_generator = KeyGenerator.new("04523", "080820")
+    key_generator.key_shift("04523")
+    key_generator.calculate_offset_from_date("080820")
+    key_generator.final_shifts("04523", "080820")
+
+    assert_equal [["v",  "w",  "x",  "y",  "z",  " ",  "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u"], ["f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  " ",  "a",  "b",  "c",  "d",  "e"], ["c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  " ",  "a",  "b"], ["e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  " ",  "a",  "b",  "c",  "d"]], decrypt_key.alphabets_for_decoding("04523", "080820")
+  end
+
 end
