41c87b9b828c4d030b85ec0906f53fdd107277ab
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-20 11:41:30 -0600

    Add test it has attributes

--- lib/decrypt_key.rb
+++ lib/decrypt_key.rb
@@ -1,6 +1,8 @@
 require './lib/key_generator'
 
 class DecryptKey < KeyGenerator
-
+  def initialize(key, date)
+    super(key, date)
+  end
 
 end
--- test/decrypt_key_test.rb
+++ test/decrypt_key_test.rb
@@ -8,4 +8,12 @@ class DecryptKeyTest < MiniTest::Test
     assert_instance_of DecryptKey, decrypt_key
   end
 
+  def test_it_has_attributes
+    encrypt_key = EncryptKey.new("04523", "080820")
+    key_generator = KeyGenerator.new("04523", "080820")
+
+    assert_equal "04523", encrypt_key.key
+    assert_equal "080820", encrypt_key.date
+  end
+
 end
