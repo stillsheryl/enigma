f8a7490c4c22db6d37f946a2c924dfcc526cbdc4
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-20 10:07:56 -0600

    Create instance variable for alphabet

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,6 +3,10 @@ require_relative 'key_generator.rb'
 
 class Enigma
 
+  def initialize
+    @alphabet = ("a".."z").to_a << " "
+  end
+
   def generate_random_key
     "%05d" % rand(99999)
   end
@@ -13,19 +17,17 @@ class Enigma
   end
 
   def find_letter_index(letter)
-    alphabet = ("a".."z").to_a << " "
-    alphabet.index(letter)
+    @alphabet.index(letter)
   end
 
   def encrypt(message, key = generate_random_key, date =  todays_date_to_string)
     encryption_info = {}
-    alphabet = ("a".."z").to_a << " "
     key_value = KeyGenerator.new(key, date)
     offset_alphabet = key_value.generate_encrypt_key(key, date)
     spaced_message = message.downcase.split("")
     encrypted_message = []
     spaced_message.each.with_index(0) do |letter, index|
-      if alphabet.include?(letter)
+      if @alphabet.include?(letter)
         encrypted_message << offset_alphabet[index % 4][find_letter_index(letter)]
       else
         encrypted_message << letter
@@ -36,13 +38,12 @@ class Enigma
 
   def decrypt(ciphertext, key, date = todays_date_to_string)
     decryption_info = {}
-    alphabet = ("a".."z").to_a << " "
     key_value = KeyGenerator.new(key, date)
     offset_alphabet = key_value.generate_decrypt_key(key, date)
     spaced_message = ciphertext.downcase.split("")
     decrypted_message = []
     spaced_message.each.with_index(0) do |letter, index|
-      if alphabet.include?(letter)
+      if @alphabet.include?(letter)
         decrypted_message << offset_alphabet[index % 4][find_letter_index(letter)]
       else
         decrypted_message << letter
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -1,5 +1,4 @@
 require_relative 'test_helper'
-require 'mocha/minitest'
 
 class KeyGeneratorTest < MiniTest::Test
 
