7c2709140c363debf91cc9cf848c6f6c69f55af5
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-20 11:28:04 -0600

    Add test it has attributes

--- test/encrypt_key_test.rb
+++ test/encrypt_key_test.rb
@@ -2,10 +2,18 @@ require_relative 'test_helper'
 
 class EncryptKeyTest < MiniTest::Test
 
-  def test_it_exists_and_has_attributes
-    encrypt_key = EncryptKey.new
+  def test_it_exists
+    encrypt_key = EncryptKey.new("04523", "080820")
 
     assert_instance_of EncryptKey, encrypt_key
+  end
+
+  def test_it_has_attributes
+    encrypt_key = EncryptKey.new("04523", "080820")
+    key_generator = KeyGenerator.new("04523", "080820")
+
+    assert_equal "04523", encrypt_key.key
+    assert_equal "080820", encrypt_key.date
   end
 
 end
