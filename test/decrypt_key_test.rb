60b53ca3aeefa2b2ac044f1e99ced2052ca8f68b
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-20 12:41:05 -0600

    Add tests to cover module methods

--- test/decrypt_key_test.rb
+++ test/decrypt_key_test.rb
@@ -9,19 +9,44 @@ class DecryptKeyTest < MiniTest::Test
   end
 
   def test_it_has_attributes
-    encrypt_key = EncryptKey.new("04523", "080820")
-    key_generator = KeyGenerator.new("04523", "080820")
+    decrypt_key = DecryptKey.new("04523", "080820")
 
-    assert_equal "04523", encrypt_key.key
-    assert_equal "080820", encrypt_key.date
+    assert_equal "04523", decrypt_key.key
+    assert_equal "080820", decrypt_key.date
+  end
+
+  def test_key_shift
+    decrypt_key = DecryptKey.new("04523", "080820")
+
+    assert_equal [4, 45, 52, 23], decrypt_key.key_shift("04523")
+  end
+
+  def test_calculate_offset_from_date
+    decrypt_key = DecryptKey.new("04523", "080820")
+
+    assert_equal [2, 4, 0, 0], decrypt_key.calculate_offset_from_date("080820")
+  end
+
+  def test_final_shifts
+    decrypt_key = DecryptKey.new("04523", "080820")
+
+    decrypt_key.key_shift("04523")
+    decrypt_key.calculate_offset_from_date("080820")
+
+    assert_equal [6, 49, 52, 23], decrypt_key.final_shifts("04523", "080820")
+  end
+
+  def test_alphabet
+    decrypt_key = DecryptKey.new("04523", "080820")
+
+    assert_equal ["g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f"], decrypt_key.alphabet(6)
   end
 
   def test_alphabets_for_decoding
     decrypt_key = DecryptKey.new("04523", "080820")
-    key_generator = KeyGenerator.new("04523", "080820")
-    key_generator.key_shift("04523")
-    key_generator.calculate_offset_from_date("080820")
-    key_generator.final_shifts("04523", "080820")
+    decrypt_key.key_shift("04523")
+    decrypt_key.calculate_offset_from_date("080820")
+    decrypt_key.final_shifts("04523", "080820")
 
     assert_equal [["v",  "w",  "x",  "y",  "z",  " ",  "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u"], ["f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  " ",  "a",  "b",  "c",  "d",  "e"], ["c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  " ",  "a",  "b"], ["e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  " ",  "a",  "b",  "c",  "d"]], decrypt_key.alphabets_for_decoding("04523", "080820")
   end
