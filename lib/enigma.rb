abca11ac2394a47c1047bba1aac4f87e7f5b1ac9
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-21 16:19:56 -0600

    Add translate message helper method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -21,38 +21,34 @@ class Enigma
     @alphabet.index(letter)
   end
 
+  def translate_message(spaced_message, offset_alphabet)
+    index = 0
+    translated_message = []
+    spaced_message.each do |letter|
+      if @alphabet.include?(letter)
+        translated_message << offset_alphabet[index % 4][find_letter_index(letter)]
+        index += 1
+      else
+        translated_message << letter
+      end
+    end
+    translated_message.join
+  end
+
   def encrypt(message, key = generate_random_key, date =  todays_date_to_string)
     encrypt_key = EncryptKey.new(key, date)
     offset_alphabet = encrypt_key.generate_encrypt_key(key, date)
     spaced_message = message.downcase.split("")
-    encrypted_message = []
-    index = 0
-    spaced_message.each do |letter|
-      if @alphabet.include?(letter)
-        encrypted_message << offset_alphabet[index % 4][find_letter_index(letter)]
-        index += 1
-      else
-        encrypted_message << letter
-      end
-    end
-    {:encryption => encrypted_message.join, :key => key, :date => date}
+    encrypted_message = translate_message(spaced_message, offset_alphabet)
+    {:encryption => encrypted_message, :key => key, :date => date}
   end
 
   def decrypt(ciphertext, key, date = todays_date_to_string)
     decrypt_key = DecryptKey.new(key, date)
     offset_alphabet = decrypt_key.generate_decrypt_key(key, date)
     spaced_message = ciphertext.downcase.split("")
-    decrypted_message = []
-    index = 0
-    spaced_message.each do |letter|
-      if @alphabet.include?(letter)
-        decrypted_message << offset_alphabet[index % 4][find_letter_index(letter)]
-        index += 1
-      else
-        decrypted_message << letter
-      end
-    end
-    {:decryption => decrypted_message.join, :key => key, :date => date}
+    decrypted_message = translate_message(spaced_message, offset_alphabet)
+    {:decryption => decrypted_message, :key => key, :date => date}
   end
 
   def crack(ciphertext, date = todays_date_to_string)
